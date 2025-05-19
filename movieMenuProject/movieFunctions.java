72e92bf0fa0315524f6f20a24f7495a76117be81
ChrisM709 <christopher.meadus@keyin.com>
2025-05-19 11:52:54 -0300

    Small Update added a method that is a seat is taken is offers the user with a new one also added some comments for clarity on what is going on in my MovieFunctions

--- movieMenuProject/movieFunctions.java
+++ movieMenuProject/movieFunctions.java
@@ -1,11 +1,13 @@
 public class MovieFunctions {
     private String[][] seats;
 
+    // starts the 10 x 10 seat layout and fills all seats as available 
     public MovieFunctions() {
         seats = new String[10][10];
         initializeSeats();
     }
 
+    // method used above to mark seats available
     public void initializeSeats() {
         for (int rowIndex = 0; rowIndex < seats.length; rowIndex++) {
             for (int columnIndex = 0; columnIndex < seats[rowIndex].length; columnIndex++) {
@@ -14,6 +16,7 @@ public class MovieFunctions {
         }
     }
 
+    // method to display seats based on them being available or not 
     public void displaySeats() {
         System.out.println("\nSeating Chart (A = Available, X = Reserved):");
         for (int rowIndex = 0; rowIndex < seats.length; rowIndex++) {
@@ -24,6 +27,7 @@ public class MovieFunctions {
         }
     }
 
+    // method to display a ticket once a seat is reserved 
     public void reserveSeat(int rowIndex, int columnIndex) {
         if (isValid(rowIndex, columnIndex)) {
             if (seats[rowIndex][columnIndex].equals("A")) {
@@ -42,12 +46,27 @@ public class MovieFunctions {
 
             } else {
                 System.out.println("\nThat seat is already reserved.");
+                findNextSeat();
             }
         } else {
             System.out.println("\nInvalid seat location.");
         }
     }
 
+    // if a seat is already taken, this method will suggest a new seat for the user from the start of the array
+    private void findNextSeat() {
+        for (int rowIndex = 0; rowIndex < seats.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < seats[rowIndex].length; columnIndex++) {
+                if (seats[rowIndex][columnIndex].equals("A")) {
+                    System.out.println("Suggested seat: Row " + (rowIndex + 1) + ", Seat " + (columnIndex + 1));
+                    return;
+                }
+            }
+        }
+        System.out.println("No Seats Found");
+    }
+
+    // method used to cancel a reservation 
     public void cancelSeat(int rowIndex, int columnIndex) {
         if (isValid(rowIndex, columnIndex)) {
             if (seats[rowIndex][columnIndex].equals("X")) {
@@ -61,6 +80,7 @@ public class MovieFunctions {
         }
     }
 
+    // checks to make sure a seat is within the array
     private boolean isValid(int rowIndex, int columnIndex) {
         return rowIndex >= 0 && rowIndex < seats.length &&
                 columnIndex >= 0 && columnIndex < seats[0].length;
