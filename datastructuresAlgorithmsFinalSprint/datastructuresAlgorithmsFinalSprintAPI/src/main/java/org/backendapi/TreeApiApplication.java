472c43577309218759ed5d6b2551bb8524718e46
Chris M <chrism709@users.noreply.github.com>
2025-08-13 13:45:17 -0300

    Updated API

---
+++ datastructuresAlgorithmsFinalSprint/datastructuresAlgorithmsFinalSprintAPI/src/main/java/org/backendapi/TreeApiApplication.java
@@ -0,0 +1,11 @@
+package org.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TreeApiApplication.class, args);
+    }
+}
